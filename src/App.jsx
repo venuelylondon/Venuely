0b56e4183b5c0627651db7b91ac5c1bc76c24103
venuelylondon <hello@venuely.london>
2026-05-19 17:48:50 +0100

    Fix App.jsx routing and add react-router-dom to dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import VenuelyLanding from './VenuelyLanding'
-import LegalPa
+import LegalPage from './LegalPage'
+
+function App() {
+  return (
+      <Routes>
+            <Route path="/" element={<VenuelyLanding />} />
+                  <Route path="/privacy" element={<LegalPage page="privacy" />} />
+                        <Route path="/terms" element={<LegalPage page="terms" />} />
+                            </Routes>
+                              )
+                              }
+
+                              export default App
